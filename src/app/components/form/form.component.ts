0140226666330582009a79143438689055172de4
akash4952 <badola4952@gmail.com>
2021-10-11 13:01:29 +0530

    frontend filter

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,760 +1,122 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ILanguage } from 'src/models/form.dto';
 import { UserService } from 'src/services/user.service';
 import Swal from 'sweetalert2';
-import { map, startWith,filter } from 'rxjs/operators';
-import { Observable} from 'rxjs';
+import { map, startWith,filter, takeUntil } from 'rxjs/operators';
+import { Observable, ReplaySubject, Subject} from 'rxjs';
+import { Area, AREAS, Outlet } from 'src/app/areas';
+import { MatSelect } from '@angular/material/select';
+
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
+
+
+
 export class FormComponent implements OnInit {
-  
+
+
+
+
   myControl = new FormControl();
   mycontro=new FormControl();
 
-  optionss=[
-    "Dalli",
-    "Sani Bar",
-    "lovely office Tatahar",
-    "Nabahi",
-    "Mohin",
-    "Fatehpur",
-    "Paunta",
-    "Barchwar",
-    "Sarkaghat Ropa",
-    "Sarkaghat bus stand",
-    "Hospital road",
-    "Jamshai",
-    "Masheran",
-    "Baradta",
-    "Asoka bar skt",
-    "Awah Devi",
-    "Cholthra",
-    "Tihara",
-    "Rakho",
-    "Vergo Bar",
-    "Ramesh Wine Parchu",
-    "Hukal",
-    "Dharampur",
-    "Santosh Office",
-    "Baroti",
-    "Chattar",
-    "Mandap",
-    "Chanota Gallu",
-    "Rissha",
-    "Allyana",
-    "Kandapattan",
-    "joginder Nager",
-    "Ladbhdol",
-    "Bashi Dhar",
-    "Sandhol",
-    "Rewalshar",
-    "Chandesh ",
-    "Durgapur",
-    "Kalkher",
-    "Patrighat",
-    "Plan Mood",
-    "Dhalwaan",
-    "Bhambla",
-    "Ekta wine Office",
-    "gandhinagar",
-    "akhara",
-    "gaman pull ",
-    "servari",
-    "dhalpur",
-    "washing",
-    "babeli",
-    "fancy guest house",
-    "Aleo",
-    "raison",
-    "dobhi",
-    "patlikulh sabjimandi",
-    "Larenkelo",
-    "patlikulh chock",
-    "nagar",
-    "nagar castle",
-    "patlikulh nagar road",
-    "naveen hotel",
-    "mall road manali",
-    "model town manali",
-    "bhang",
-    "vashist",
-    "palchan",
-    "solangnala",
-    "bajaura",
-    "jeedi",
-    "nagwain",
-    "takoli",
-    "panarsa",
-    "banala",
-    "aut-1",
-    "aut-2",
-    "hathi than",
-    "sarsadi",
-    "SHAT",
-    "JARI",
-    "OLD KASOL",
-    "peerdi",
-    "KASOL",
-    "bhuntar sabjimandi",
-    "parla bhuntar",
-    "banjar sabjimandi",
-    "MANIKARAN",
-    "Teju ofc.thunag",
-    "karosg-2",
-    "luhri",
-    "jagat khana",
-    "nither",
-    "pandoh",
-    "indra market",
-    "khaliyar",
-    "Pashupatti Ent.",
-    "GANDHI CHOWK",
-    "TIKKAR",
-    "TAUNI DEVI",
-    "CHOWKI  ",
-    "KUTHERA",
-    "SUJJANPUR",
-    "KERMI CHOWK",
-    "MUNDKHAR",
-    "LADROOR KLAAN",
-    "HARSOR",
-    "JARLI",
-    "MEHRE",
-    "JHANIARI",
-    "NADOUN B.STAND",
-    "GWAAL PATHER",
-    "jhalera",
-    "united L-1",
-    "rakkar",
-    "santokhgarh",
-    "monu office",
-    "keori",
-    "madan office",
-    "ghanari",
-    "sangnai",
-    "kinnu",
-    "royal bar",
-    "khurwain",
-    "padyalag",
-    "kandraur chowk",
-    "rajesh office",
-    "madanpur basoli",
-    "Shamsher Office",
-    "tahliwal",
-    "vishal office",
-    "lakar bazar main",
-    "khawara chawki  shoghi  ",
-    "darnika bagicha",
-    "dhaliman tunnel",
-    "janedhghat",
-    "kasumpti",
-    "tutupower house",
-    "dhami",
-    "BAGH BANIA",
-    "BAROTIWAL",
-    "BADDI BUS STAND",
-    "BASANTI BAGH",
-    "ALKEM",
-    "AMIL PHARMA",
-    "BHOGPUR",
-    "NAWA GAON",
-    "BHEEDI JHODI",
-    "DABOUR",
-    "Bundla",
-    "bindraban",
-    "Mahakal",
-    "Itchi",
-    "Beerta",
-    "JADRANGEL ",
-    "Sidbadi",
-    "Civil Lines",
-    "ramnager",
-    "tanda",
-    "Sai",
-    "Dehra BS",
-    "CHAMUNDA ENTERPRISES L-1 CHAMBA",
-    "KANGRA WINES L-1 YOL",
-    "VRV LIQUOR L-1 KANGRA",
-    "Goru",
-    "CHHRORNALA",
-    "bhuntar bus stand",
-    "garsa chock",
-    "banjar bustand",
-    "Pandoh",
-    "Bablu ofc.chatri",
-    "karsog -3",
-    "Anni",
-    "anni ofc",
-    "jagat khana",
-    "saulikhad",
-    "Mandi bus stand",
-    "katola",
-    "United Kullu",
-    "HMR B.STAND",
-    "BHOTA",
-    "BHATAID",
-    "CHABUTRA",
-    "SACHUI",
-    "TARAKWADI",
-    "NBS JAHHOO",
-    "MAIR",
-    "DHANGOTA",
-    "SUKKAR KHAD",
-    "GALOD",
-    "United Hmr",
-    "MAANPUL",
-    "RANGAS 2",
-    "o.b.s una",
-    "nangal road una",
-    "dehlaan",
-    "mazara",
-    "Khad",
-    "pandoga",
-    "dillon office",
-    "nangal jariala",
-    "amb",
-    "gangunala",
-    "sarbjit office",
-    "bangana",
-    "thakur L-1",
-    "G.N suri L-1",
-    "bamta",
-    "berthin",
-    "anil bar",
-    "changer",
-    "ghaluwal",
-    "bathu",
-    "mall road main",
-    "cart raod",
-    "shoghi",
-    "kanlog",
-    "chotta shimla",
-    "lamidhar",
-    "bcs",
-    "tutumain",
-    "bemloi",
-    "KISHANPURA",
-    "BURAWALA",
-    "BALD NADI",
-    "PDM CHOWK",
-    "CHOWKIWALA",
-    "DHEROWAL",
-    "PANJERA",
-    "GURU KUND",
-    "JOGON",
-    "KHARUNI",
-    "Rajpur",
-    "paprola",
-    "avari",
-    "Kachiari",
-    "OBS Kangra",
-    "sidpur",
-    "Kotwali",
-    "Fursatdganj",
-    "pahra",
-    "Daroh Circle",
-    "Petrol Pump",
-    "Balbir JI office",
-    "GOLDY WINE L-1 NURPUR",
-    "PALAMPUR WINE TRADERS L-1 PALAMPUR",
-    "JASWANT WINE L-1",
-    "Prince katoch",
-    "Manikaran",
-    "flight view",
-    "larji",
-    "Uper bunjar",
-    "Thunag",
-    "Janjehli-2",
-    "Sanarli",
-    "Anni-2",
-    "Nirmand",
-    "chauki",
-    "hara bagh",
-    "mandi main",
-    "IIT kamand",
-    "PACCA BHARO",
-    "BHOTA CHOWK",
-    "UHAL",
-    "AMROH",
-    "BALAIT",
-    "BOHNI",
-    "BASSI",
-    "CHAHAB",
-    "BAGTU",
-    "BIJRI",
-    "SALOUNI",
-    "NALTI",
-    "BHOMPAL",
-    "KANGOO",
-    "KAKRU",
-    "L-2 una",
-    "L-1 FAQIR chand",
-    "roopsi bar",
-    "uday pur",
-    "nagnoli",
-    "mawa sidiya",
-    "gagret",
-    "daulatpur",
-    "mubarikpur",
-    "karluhi",
-    "Lal singi",
-    "danggar",
-    "BHAGER",
-    "GHAGUS",
-    "chandpur",
-    "mehfil bar",
-    "dhusara",
-    "jol",
-    "nangal khurd",
-    "Manish office",
-    "lower bazar tuneal",
-    "bhrari",
-    "tutikandi",
-    "dhaliman",
-    "koti",
-    "panthaghati",
-    "baluganj",
-    "dumb",
-    "KHERA",
-    "KALAYANPUR",
-    "KATHA",
-    "CHAKKAN ROAD",
-    "BILAANWALA",
-    "MANJHOLI",
-    "CHIKNI PULL",
-    "MASTAANPURA",
-    "NAYI SARLI",
-    "THELLA",
-    "Guggar",
-    "menjha",
-    "Bir",
-    "Jamanabad",
-    "Barod Rd ",
-    "ikku kad",
-    "fatehpur",
-    "Sidpur",
-    "panchrukki ",
-    "deogran ",
-    "Thoral",
-    "Dehra",
-    "CHAMBA ENTERPRISES L-1 CHAMBA",
-    "HIMALAYAN WINE TRADERS L-1 PALAMPUR",
-    "SUPER ASSOCIATES RAMP L-1",
-    "Brar",
-    "Parvesh",
-    "AGGARWALL WINE L-1 NAHAN",
-    "mohal",
-    "bali choki 2",
-    "bunjar",
-    "Janjehli",
-    "kelodhar",
-    "Sanarli ofc",
-    "brow",
-    "Nirmand ofc.",
-    "Omkaar ent.",
-    "purani mandi",
-    "Mandi-4",
-    "Rali",
-    "Kehar wine",
-    "BHEERA",
-    "KALANJHARI",
-    "PURLI",
-    "PATLANDER",
-    "LAMBLOO",
-    "SULGWAAN",
-    "KHUTHRIN",
-    "SUHARI",
-    "CHAKMOH",
-    "BANI",
-    "BAJORI",
-    "BELA",
-    "DOGI",
-    "WARD NO.3",
-    "hsp road una",
-    "rana wine",
-    "mehatpur bazar",
-    "gagan office",
-    "badehra rajputa",
-    "bharsali",
-    "gagret brder",
-    "madwara",
-    "andora",
-    "anmol bar",
-    "jol ",
-    "ghumarwin",
-    "kandraur",
-    "AMA L-1",
-    "sungal",
-    "sunhani",
-    "chururu",
-    "mandli",
-    "badehra ",
-    "bathri",
-    " mall road lift",
-    "lakar bazar bus stand",
-    "taradevi",
-    "khailini",
-    "kufri",
-    "bhattkuffer",
-    "new shimla",
-    "lowertutu",
-    "NALAGARH",
-    "MALKHU MAJRA",
-    "EXPORT PARK",
-    "BALD NADI 2",
-    "BIRLA TEXTILE",
-    "RAJPURA",
-    "BHATIAN",
-    "KARSOLI",
-    "RAMSHER 1",
-    "MASTAANPURA",
-    "SABZI MANDI",
-    "Old Bus Stand",
-    "Baijnath",
-    "78mils",
-    "Nagrota",
-    "gopalpur",
-    "sidwari",
-    "Rakkar",
-    "Temple Rd",
-    "shivnager",
-    "Nagni",
-    "Bawarna",
-    "Sonu Office",
-    "HIMALAYAN WINE AGENCY L-1 DEHRA",
-    "PLATINUM TRADERS L-1 SAKOH",
-    "RED BOTTLE L-13",
-    "Agar singh rana",
-    "Ramesh chand",
-    "samshi",
-    "bali choki",
-    "takoli",
-    "chattri",
-    "karsog",
-    "kamal g ofc.krsog",
-    "bayal",
-    "aani bus stand",
-    "jail road",
-    "college road",
-    "des raj ofc ",
-    "Chadiyar",
-    "Subhash ji office",
-    "DOSARKA ",
-    "GAWARDU",
-    "KAKKAR",
-    "Sarita Verma office",
-    "JANGALBERI",
-    "DERA PAROL",
-    "JAHHOO",
-    "PATTA",
-    "SAMTANA",
-    "GARLI",
-    "BARSAR",
-    "RANGAS",
-    "BAGHNALA",
-    "DHANETA",
-    "police line",
-    "L-1 hemraj dogra",
-    "behdala",
-    "santokhgarh border",
-    "nardeep office",
-    "badoh",
-    "sanjeev office",
-    "chalet",
-    "ajay office",
-    "bharwain",
-    "sanjeev office",
-    "chilli",
-    "dakri chowk",
-    "bus stand bilaspur",
-    "luhnoo",
-    "talai",
-    "baduhi",
-    "thanakallan",
-    "haroli",
-    "laluwal",
-    "lower bazar ",
-    "kaithu",
-    "kachighati",
-    "sanjauli",
-    "chini banglow",
-    "mehli",
-    "Vikasnagar",
-    "ghanahatti",
-    "KIRPALPUR",
-    "BHUDD",
-    "JHARMAJRI",
-    "SAI ROAD",
-    "VARDHMAAN",
-    "NATHU KALAAN",
-    "SALLEWAL",
-    "TIKKRI",
-    "RAM SHER B.STAND",
-    "THANA ",
-    "PNB BANK",
-    "Petrol Pump",
-    "Chogan",
-    "kandwari",
-    "Garg Cly",
-    "dhad",
-    "sheela chowk ",
-    "Shila Chowk",
-    "malnu",
-    "khera",
-    "Pudwa",
-    "Bairghata ",
-    "Jaswant wine",
-    "HIMALAYAN WINE ENTERPRISES KACHHIARI L-1",
-    "RANA WINE L-1 KANGRA",
-    "Gulzar",
-    "Khundiya",
-    "Amar singh",
-    "Sunil kapoor",
-    "AMA ENTERPRISES L-1 BILASPUR",
-    "HAMIR KEHAR WINE L-1 HAMIRPUR",
-    "RANA WINE L-1 UNA",
-    "Shinda group",
-    "Vinod agarwal",
-    "Sanjeev mubarakpur",
-    "AD  TRADERS L-13 SHIMLA",
-    "HIMALAYAN COUNTRY LIQUOR L-13  KACHHARI",
-    "RAM PARKASH L-13 SOLAN",
-    "COPIA BEVERAGES L-3 BADDI",
-    "SAROJ KUMARI L-13",
-    "Goldy WINE L-13 NURPUR",
-    "Janoghat",
-    "Bithal",
-    "Chaudhary Adda",
-    "Sadhupul",
-    "Rajput wines",
-    "kumarhatti",
-    "Tipra",
-    "Gamberpul",
-    "Manan garg",
-    "Deha",
-    "Nerwa ofc.-2",
-    "patsari",
-    "Samala",
-    "Badiyara",
-    "kotkhai",
-    "Theog main",
-    "Mandi Bus stand",
-    "Shallaghat",
-    "lovely ofc.",
-    "S.S.Wine",
-    "BCS Sharma",
-    "Sarwari Shop",
-    "Manali main shop",
-    "R.K.saini",
-    "COPIA BEVERAGES L-1 BADDI",
-    "HARMAN WINE L-1 BADDI",
-    "THAKUR WINE L-1 GHUMARWIN",
-    "Rajkumar group",
-    "Shamsher singh",
-    "Gagan",
-    "ANGRA ENTERPRISES L-13 UNA",
-    "KALTA SPIRITS L-13 SHIMLA",
-    "RANA WINE L-13 PALAMPUR",
-    "HIMACHAL LIQUAR L-13",
-    "RED BOTTLE L-13",
-    "Rajesh & co. L-13 Bilaspur",
-    "Narkanda ",
-    "Nogli",
-    "Gulati Ji office",
-    "Salogra",
-    "chd.wines",
-    "Sukki jodi",
-    "kasauli",
-    "Kunihar ofc.",
-    "Arki ofc",
-    "Nerwa",
-    "pandranu",
-    "Boutinala",
-    "Raju negi ofc",
-    "chirgaon",
-    "chaila",
-    "Mandi",
-    "Bhararighat",
-    "AMA ENTERPRISES L-1 SUNDER NAGAR",
-    "Sarkaghat bs",
-    "Kalta wine",
-    "A.D.tarder",
-    "Pashupatti Ent. Office",
-    "Gumpa road",
-    "FAQUIR CHAND & SONS L-1 UNA",
-    "HEMRAJ DOGRA L-1 UNA",
-    "UNITED LIQOUR L-1 HAMIRPUR",
-    "Jitender rana group",
-    "Rajesh & co.",
-    "Neeraj thakur",
-    "ASHWANI KUMAR L-13 SUNDERNAGAR",
-    "KANGRA WINE L-13 YOL",
-    "VEENA KUMARI L-13 PALAMPUR",
-    "KALASH WINE L-13",
-    "Durga ent.",
-    "sheenu ent,",
-    "kumarsen",
-    "Rampur",
-    "Jaggu Bar Narkanda",
-    "Subzy mandi",
-    "Bye pass",
-    "parwanu",
-    "kasauli ofc",
-    "Gamberpul ofc ",
-    "Theog",
-    "Chopal ofc",
-    "kuddu",
-    "Rohru main",
-    "Ranjeet ofc.",
-    "jubbal",
-    "Janoghat",
-    "Des Raj ofc.",
-    "Hara Bagh",
-    "kalkhar",
-    "Royal wine",
-    "Gulat Ram",
-    "Shoghi Off",
-    "United Office kullu",
-    "Old MANALI",
-    "GURMEET SINGH AND OTHERS L-1 BADDI",
-    "HIMALAYAN WINE TRADERS L-1 HAMIRPUR",
-    "UNITED TRADERS L-1 UNA",
-    "Joy wine",
-    "Jaggu",
-    "Madan jaswal",
-    "BABITA DEVI L-13",
-    "MOHIT DHALL L-13 CHAMBA",
-    "HARPAL SINGH L-13",
-    "MAHAKALI ENT. L-13",
-    "Lovely Ent.",
-    "Sandhu",
-    "Nirath",
-    "Rampur Office",
-    "kandaghat",
-    "Rupi g ofc",
-    "Dharampur",
-    "Parwanu ofc.",
-    "Kunihar",
-    "Arki",
-    "chopal",
-    "Anti",
-    "Sen singh ofc",
-    "kalta g ofc.",
-    "Sachin negi ofc",
-    "Gumma",
-    "Kufri ofc.",
-    "Darlaghat",
-    "Sundernagar",
-    "Nilu ofc",
-    "B.D.wine",
-    "Parmar",
-    "Omkaar ent. Office",
-    "Model shop manali",
-    "GURNARAIN SURI AND COMPANY L-1 BILASPUR",
-    "RANA WINE L-1 BILASPUR",
-    "SARITA VERMA L-1",
-    "Agarwal group",
-    "Majithiya Group",
-    "Ankush gupta",
-    "HAMIR  WINE L-13 HAMIRPUR",
-    "RAJEEV PATIAL L-13 UNA",
-    "KUNDLAS WINE L-13",
-    "KANTA VERMA L-13",
-    "Tilak raj L-13",
-    "Shalli wine L-13",
-    "kingal",
-    "jagatkhana",
-    "chail",
-    "chamabaghat",
-    "Saprun",
-    "parwanu berrier",
-    "subathu",
-    "subathu ofc",
-    "Sainj",
-    "nerwa ofc.",
-    "Sawra",
-    "Rohru",
-    "Chirgaon road",
-    "khada pathar",
-    "janoghat ofc",
-    "Des Raj l-1",
-    "Chamakripul",
-    "Sarkaghat",
-    "Paradise wine",
-    "Bushar wine",
-    "Dhalpur shop",
-    "Bhunter Shop",
-    "Ekta wine Office"
-];
+ 
 
-addresss=[
-  "English Wine & Beer Shop",
-  "Nillu ji Office",
-  "English wine shop,gandhinagar",
-  "English wine shop,akhara",
-  "English wine shop,gaman pull ",
-  "English wine shop,servari",
-  "English wine shop,dhalpur",
-  "English wine shop,washing",
-  "English wine shop,babeli",
-  "English wine shop,fancy guest house",
-  "English wine shop,aleo",
-  "English wine shop,raison",
-  "English wine shop,dobhi",
-  "English wine shop,patlikulh sabjimandi",
-  "English wine shop,Larenkelo",
-  "English wine shop,patlikulh chock",
-  "English wine shop,nagar",
-  "English wine shop,nagar castle",
-  "English wine shop,patlikulh nagar road",
-  "English wine shop,naveen hotel",
-  "English wine shop,mall road",
-  "English wine shop,model town",
-  "English wine shop,bhang",
-  "English wine shop,vashist",
-  "English wine shop,palchan",
-  "English wine shop,solangnala",
-  "English wine shop,BAJAURA",
-  "English wine shop,flight view",
-  "English wine shop,bhuntar bustand",
-  "English wine shop,manikarn chock",
-  "English wine shop,garsa road",
-  "English wine shop,garsa",
-  "English wine shop,samshi ",
-  "English wine shop,mohal",
-  "English wine shop,hathi than",
-  "English wine shop,sarsadi",
-  "English wine shop,SHAT",
-  "English wine shop,JARI",
-  "English wine shop,OLD KASOL",
-  "peerdi",
-  "English wine shop,KASOL",
-  "bhuntar sabjimandi",
-  "parla bhuntar",
-  "English wine shop,banjar sabjimandi",
-  "English wine shop,MANIKARAN",
-  "English wine shop",
-  "Babeli",
-  "English wine shop,CHHRORNALA",
-  "bhuntar bus stand",
-  "garsa chock",
-  "English wine shop,banjar bustand",
-  "Ramshilla",
-  "English wine shop,Manikaran",
-  "flight view",
-  "English wine shop,larji",
-  "English wine shop,bunjar",
-  "mohal",
-  "English wine shop,bali choki",
-  "Kullu Main,Sarvari",
-  "samshi",
-  "English wine shop,akoli"
+
+
+  protected areas: Area[] = AREAS;
+  public areaCtrl: FormControl = new FormControl();
+  public areaFilterCtrl: FormControl = new FormControl();
+  public filteredAreas: ReplaySubject<Area[]> = new ReplaySubject<Area[]>(1);
+  @ViewChild('singleSelect', {static: true}) singleSelect: MatSelect;
+  
+  protected _onDestroy = new Subject<void>();
+
+  OUTLETS:Outlet[]= [
+   
 ]
+
+  
+  sortedOutlets = []; 
+  pushOutlets(){
+    this.sortedOutlets = this.areaCtrl.value.outlets;
+    console.log(this.sortedOutlets)
+    this.sortedOutlets.map(x => this.OUTLETS.push({outletName:x}))
+    
+  }
+
+
+
+
+
+ 
+
+  
+
+
+
+protected outlets: Outlet[] = this.OUTLETS;
+public outletCtrl: FormControl = new FormControl();
+public outletFilterCtrl: FormControl = new FormControl();
+public filteredOutlets: ReplaySubject<Outlet[]> = new ReplaySubject<Outlet[]>(1);
+
+
+
+
+  
+  // sortOutlets(){
+  //   if (this.areaCtrl.value.areaName==)
+  // }
+
+  protected filterAreas() {
+    if (!this.areas) {
+      return;
+    }
+
+  let search = this.areaFilterCtrl.value;
+  if (!search) {
+    this.filteredAreas.next(this.areas.slice());
+    return;
+  }else {
+    search = search.toLowerCase();
+  }
+
+  this.filteredAreas.next(
+    this.areas.filter(area => area.areaName.toLowerCase().indexOf(search) > -1 )
+  );
+
+  }
+
+
+  protected filterOutlets() {
+    if (!this.outlets) {
+      return;
+    }
+    let search = this.outletFilterCtrl.value;
+    if (!search) {
+      this.filteredOutlets.next(this.outlets.slice());
+      return;
+    }else {
+      search = search.toLowerCase();
+    }
+  
+    this.filteredOutlets.next(
+      this.outlets.filter(outlet => outlet.outletName.toLowerCase().indexOf(search) > -1)
+    )
+  }
+
+
+  
+
+
+
+
+
   
  filteredOptions: Observable<string[]>;
  filteredOption: Observable<string[]>;
@@ -775,40 +137,41 @@ addresss=[
   options: Observable<string[]> = this.userService.getAllOutlets();
 
   addresses: Observable<string[]> = this.userService.getAllAddresses();
+
+  
+
+
  
+
+
   ngOnInit(): void {
     this.userService.checkUuid(this.params).subscribe(data => {
       if(data.status===true){
         this.router.navigate([`/form-registered/${this.language}`]);
       }
+
+      
          
    
     });
-    
-    this.filteredOptions = this.myControl.valueChanges
-    .pipe(
-      startWith(''),
-      map(value =>{
-       const filterValue=value.toLowerCase();
-        return this.optionss.filter(option => option.toLowerCase().includes(filterValue));
-      })
-    );
-    
-       
-      
-      this.filteredOption = this.mycontro.valueChanges
-      .pipe(
-        startWith(''),
-        map(value =>{
-         const filterValue=value.toLowerCase();
-          return this.addresss.filter(option => option.toLowerCase().includes(filterValue));
-        })
-      );
-  
+        
+   this.filteredAreas.next(this.areas.slice());
+
+   this.areaFilterCtrl.valueChanges
+   .pipe(takeUntil(this._onDestroy))
+   .subscribe(()=>{
+     this.filterAreas();
+   })
    
-    
-  
-  }
+
+   this.outletFilterCtrl.valueChanges
+   .pipe(takeUntil(this._onDestroy))
+   .subscribe(()=> {
+     this.filterOutlets();
+   } );
+
+
+   }
 
   
  
@@ -947,3 +310,7 @@ addresss=[
 
   
 }
+function x(x: any, arg1: (string: any) => { x: any; }): string {
+  throw new Error('Function not implemented.');
+}
+
